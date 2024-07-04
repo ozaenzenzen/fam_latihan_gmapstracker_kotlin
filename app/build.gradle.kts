cb9ed2a4ee8de26d85292dfa6a8332041c578028
Fauzan Akmal Mahdi <recovery252@gmail.com>
2024-07-04 15:17:08 +0700

    update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,4 +49,8 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+
+    // Google Maps
+    implementation("com.google.android.gms:play-services-maps:19.0.0")
+    implementation("com.google.android.gms:play-services-location:19.0.0")
 }
